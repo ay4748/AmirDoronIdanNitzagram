05a58d12b60e9e4a910d3704fc29ac42641f4948
ay4748 <ay4748@bs.amalnet.k12.il>
2025-02-03 15:19:40 +0200

    finished image post

--- classes/ImagePost.py
+++ classes/ImagePost.py
@@ -4,11 +4,13 @@ from helpers import screen
 
 
 class ImagePost(Post):
-    def __init__(self, username, location, description, image_path):
+    def __init__(self, username, location, description, image):
         super().__init__(username, location, description)
-        self.image_path = image_path
+        self.image = image
 
     def display(self):
-        img = pygame.image.load(image_path)
+        img = pygame.image.load(self.image)
         img = pygame.transform.scale(img, (POST_WIDTH, POST_HEIGHT))
-        screen.blit(img, (POST_X_POS,POST_Y_POS))
+        screen.blit(img, (POST_X_POS, POST_Y_POS))
+
+        super().display()
